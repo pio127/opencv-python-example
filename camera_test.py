46b59692368b9603c497ca6ba4c4504ea3e73a78
pio127 <38538752+pio127@users.noreply.github.com>
2019-03-16 18:02:01 +0100

    Create camera_test.py

---
+++ camera_test.py
@@ -0,0 +1,14 @@
+import cv2
+
+camera = cv2.VideoCapture(0)
+if camera.isOpened():
+    while True:
+        success, frame = camera.read()
+        if success:
+            cv2.imshow("test", frame)
+            if cv2.waitKey(1) == 27:
+                break
+        else:
+            print("Unable to grab frame")
+            break
+cv2.DestroyAllWindows()
